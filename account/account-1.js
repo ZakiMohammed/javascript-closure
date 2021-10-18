84a97f0d2cd50bbc3caf8c87b69947110cbeb609
zaki.shaikh <zaki.shaikh@vergehealth.com>
2021-10-18 03:27:41 -0500

    HTML file added, made compatible for both Node.js and HTML

--- account/account-1.js
+++ account/account-1.js
@@ -13,4 +13,23 @@ function account(number, type, baseAmount = 0) {
     };
 }
 
-module.exports = account;
+const account1 = account(2001, 'Savings', 1000);
+
+console.log('Number:', account1.getNumber());
+console.log('Type:', account1.getType());
+console.log('Balance:', account1.getBalance());
+
+console.log('Balance:', account1.withdraw(200));
+console.log('Balance:', account1.deposit(300));
+console.log('Balance:', account1.deposit(300));
+console.log('Balance:', account1.withdraw(30000));
+console.log();
+
+const account2 = account(4001, 'Current', 500);
+
+console.log('Number:', account2.getNumber());
+console.log('Type:', account2.getType());
+console.log('Balance:', account2.getBalance());
+
+console.log('Balance:', account2.withdraw(200));
+console.log('Balance:', account2.deposit(1000));
--- settimeout/settimeout-6.js
+++ settimeout/settimeout-6.js
@@ -1,17 +1,17 @@
-// setTimeout can remember variables from its lexical scope even they execute on different timeline
+// setTimeout can remember variables from its lexical scope even they execute on different timeline (arrow)
 
 let value0 = 1;
 
-function level1() {
+const level1 = () => {
     let value1 = 10;
-    return function () {
+    return () => {
         let value2 = 100;
-        return function () {
+        return () => {
             let value3 = 1000;
-            return function () {
+            return () => {
                 let value4 = 10000;
             
-                setTimeout(function () {
+                setTimeout(() => {
                     let value5 = 100000;
                     console.log(value0, value1, value2, value3, value4, value5);
                 }, 1000);
@@ -19,7 +19,6 @@ function level1() {
         }
     }
 }
-
 const level2 = level1();
 const level3 = level2();
 const level4 = level3();
